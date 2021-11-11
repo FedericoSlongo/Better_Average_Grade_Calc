ac26999c0e90630d4d191b3abf42e0413c9d7a0a
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-11-11 19:01:56 +0100

    Adding the files that actually work

---
+++ Average_Grage_Double/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_Grage_Double
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ciclo = 0;
+            double voto, voto_temp = 0, voto_finale;
+            bool error = false;
+            do
+            {
+                Console.WriteLine("Insert the grade ");
+                do
+                {
+                    if (error)
+                    {
+                        Console.WriteLine("The grade you inserted isn't valid");
+                    }
+                } while (error = !double.TryParse(Console.ReadLine(), out voto));
+                voto_temp = voto_temp + voto;
+                if (voto == 0)
+                {
+                    Console.Clear();
+                    voto_finale = voto_temp / ciclo;
+                    Console.WriteLine($"The final grade is {voto_finale}");
+                }
+                ciclo++;
+            } while (!(voto == 0));
+            Console.ReadKey();
+        }
+    }
+}
